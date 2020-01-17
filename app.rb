e21b5dcdff92c0c408554adf763cd1592d923f5a
cpcwood <cpcwood@outlook.com>
2020-01-17 14:19:11 +0000

    refactor pages

--- app.rb
+++ app.rb
@@ -3,23 +3,27 @@ require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
 
+  get '/' do
+    erb :homepage
+  end
+
+  get '/bookmarks' do
+    erb :index
+  end
+
+  get '/bookmarks/new' do
+    erb :new
+  end
+
   post '/bookmarks' do
-    p ENV
-
-    @bookmarks = Bookmarks.all
-
     url = params['url']
     connection = PG.connect(dbname: 'bookmark_manager_test')
     connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
     redirect '/bookmarks'
   end
 
-  get '/bookmarks/new' do
-    erb :'bookmarks/new'
-  end
-
-  get '/bookmarks' do
-    erb :'bookmarks/index'
+  before do
+    @bookmarks = Bookmarks.all
   end
 
   run! if app_file == $0
